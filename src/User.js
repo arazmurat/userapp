9d5d502fa8589a5564143bcf253cf8362c4f315d
Arazmyrat <arazmirat@icloud.com>
2021-04-20 23:08:06 +0400

    add useContext

--- src/User.js
+++ src/User.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React,{useContext} from 'react'
+import {UserContext} from './context/UserContext'
 
-function User({ user, changeColor }) {
-  return (
-    <div style={{ background: user.color }}>
-      <h3>{user.name}</h3>
-      <p>Email: {user.email}</p>
-      <p>Age: {user.age}</p>
-      Color: <input value={user.color} onChange={e=>changeColor(user.id,e.target.value)} />
-    </div>
-  );
+function User({user}) {
+    const {changeColor} = useContext(UserContext)
+    console.log("DataConn:",changeColor)
+    return (
+        <div style={{background:user.color}}>
+            <h3>{user.name}</h3>
+            <p>Email: {user.email}</p>
+            <p>Age: {user.age}</p>
+            Color: 
+            <input value={user.color} 
+                onChange={e=>changeColor(user.id,e.target.value)} />
+        </div>
+    )
 }
 
-export default User;
+export default User
